69600fc598de5b416145658fbfd8472abee00705
vdalex <alex_house@ukr.net>
2023-06-27 06:37:28 +0300

    Audio format conversion added to work with aac, ogg and opus

--- whisper_api/whisper_demo_api.py
+++ whisper_api/whisper_demo_api.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 import os
+import io
 import gradio as gr
 import openai
 from datetime import datetime
+from subprocess import CalledProcessError, run
 import json
 import re
 
@@ -27,18 +29,38 @@ if os.path.isfile("replacements.json"):
     with open("replacements.json", 'r', encoding='utf-8') as file:
         replacement_data = json.load(file)
 
+
 def replaceText(transcribtion_text):
     for key in replacement_data.keys():
-        transcribtion_text = re.sub(r'(?i)'+key, replacement_data[key], transcribtion_text)
-    
+        transcribtion_text = re.sub(
+            r'(?i)'+key, replacement_data[key], transcribtion_text)
+
     return transcribtion_text
+
+
+def convertAudio(audio):
+    cmd = [
+        "ffmpeg",
+        "-nostdin",
+        "-threads", "0",
+        "-i", audio,
+        "-ac", "1",
+        "-acodec", "libmp3lame",
+        "-f", "mp3",
+        "pipe:1"
+    ]
+    try:
+        out = run(cmd, capture_output=True, check=True).stdout
+    except CalledProcessError as e:
+        raise RuntimeError(f"Failed to load audio: {e.stderr.decode()}") from e
+    return out
 
 
 def sendToWhisper(audio_upload, results):
     start, result = datetime.now(), [None, None, None]
 
     if audio_upload is None:
-        #return "ERROR: You have to upload an audio file"
+        # return "ERROR: You have to upload an audio file"
         return [["No input"]*3]
 
     audio = audio_upload
@@ -52,17 +74,24 @@ def sendToWhisper(audio_upload, results):
 
     results.append(result)
     print(f"task started for: {file_name}")
-    audio_data = open(audio, 'rb')
-    transcription = openai.Audio.transcribe(MODEL_NAME, audio_data, language=LANG, prompt=initial_prompt_data)
+
+    # openai.Audio supports m4a, mp3, webm, mp4, mpga, wav, mpeg formats and it looks first format check
+    # is just file extension check. So let just name file as "audio.mp3" and converts all audio data to mp3
+    audio_data = io.BytesIO(convertAudio(audio))
+    audio_data.name = "audio.mp3"
+    transcription = openai.Audio.transcribe(
+        MODEL_NAME, audio_data, language=LANG, prompt=initial_prompt_data)
 
     if replacement_data:
         text = replaceText(transcription["text"])
     else:
         text = transcription["text"]
-    
-    result[0], result[1], result[2] = file_name, text, str((datetime.now() - start).total_seconds())
+
+    result[0], result[1], result[2] = file_name, text, str(
+        (datetime.now() - start).total_seconds())
     print(f"task completed for: {file_name}")
     return results
+
 
 CSS = """
 #audio_inputs {
@@ -74,22 +103,27 @@ CSS = """
 with gr.Blocks(css=CSS) as demo:
 
     gr.Markdown("### [OpenAI Whisper](https://openai.com/blog/whisper/)  Demo")
-    gr.Markdown("A [Gradio](https://gradio.app) based Speech-to-Text (aka ASR) Demo of the [Open AI API](https://github.com/openai/openai-python)")
-    
+    gr.Markdown(
+        "A [Gradio](https://gradio.app) based Speech-to-Text (aka ASR) Demo of the [Open AI API](https://github.com/openai/openai-python)")
+
     results = gr.State([])
     with gr.Column():
 
         gr.Markdown("### Upload audio")
 
         with gr.Row():
-            audio_upload = gr.Audio(source="upload", type="filepath", interactive=True, elem_id="audio_inputs")
+            # Gradio supports formats for player
+            audio_upload = gr.Audio(
+                source="upload", type="filepath", interactive=True, elem_id="audio_inputs")
 
         with gr.Row():
             submit = gr.Button(value="Transcribe")
 
         gr.Markdown("### Result")
-        output = gr.Dataframe(headers=["Filename", "Transcription", "Time [s]"], label="Results", wrap=True)
+        output = gr.Dataframe(
+            headers=["Filename", "Transcription", "Time [s]"], label="Results", wrap=True)
 
-    submit.click(fn=sendToWhisper, inputs=[audio_upload, results], outputs=output)
+    submit.click(fn=sendToWhisper, inputs=[
+                 audio_upload, results], outputs=output)
 
 demo.launch(server_name="0.0.0.0", server_port=7860, share=False)
